4f931ca2dba8d7dc9ac1b7b009dbe1dbc2902e71
Peter Komaromi <kompeet1@gmail.com>
2020-04-28 10:18:07 +0000

    added sound variables

---
+++ assets/javascript/main.js
@@ -0,0 +1,5 @@
+//--Sound Variables
+const greenBtnSound = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3");
+const yellowBtnSound = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3");
+const redBtnSound = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3");
+const blueBtnSound = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3");
